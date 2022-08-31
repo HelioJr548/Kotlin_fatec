f32680fe3f18c0bf92c49cc13249146b90b770f2
HelioJr548 <heliojr548@gmail.com>
2022-08-31 15:50:13 -0300

    Terceiro exercicio da 2° atividade, finalizado

--- src/main/kotlin/Atividade02/main.kt
+++ src/main/kotlin/Atividade02/main.kt
@@ -2,7 +2,8 @@ package Atividade02
 
 fun main() {
 //    OrdemCrescente()
-    MediaAluno()
+//    MediaAluno()
+    MaiorDe3()
 }
 
 //  leia 3 (três) valores inteiros e
@@ -22,25 +23,25 @@ fun OrdemCrescente() {
 //  se o valor da média escolar for maior ou igual a 7.
 //  Se o aluno não foi aprovado, indicar uma mensagem informando esta condição
 fun MediaAluno() {
-    val n = mutableListOf<Double>()    //cria lista
-    while (n.size != 4) {   // enquanto tamanho da lista(n) for difernte de 4
-        print("Nota: ") //mensagem para usuario
-        val x = readLine().toString().toDouble()    //variavel para input
-        if (x < 0 || x > 10) {  //estrutura condicional para notas maiores que 10 ou menor que 0
-            println("Valor Inválido, apenas número entre 0 e 10")   //mensagem caso condição for Verdadeira
-        } else {    //caso condição for Falsa
-            n += x  //insere valores digitados na lista
+    val n = mutableListOf<Double>()    //Cria lista
+    while (n.size != 4) {   // Enquanto tamanho da lista(n) for difernte de 4
+        print("Nota: ") //Mensagem para usuario
+        val x = readLine().toString().toDouble()    //Variavel para input
+        if (x < 0 || x > 10) {  //Estrutura condicional para notas maiores que 10 ou menor que 0
+            println("Valor Inválido, apenas número entre 0 e 10")   //Mensagem caso condição for Verdadeira
+        } else {    //Caso condição for Falsa
+            n += x  //Insere valores digitados na lista
         }
     }
     val media: Double =
-        n.sumOf { it } / n.size //calcula média dividindo soma do conteudo da lista por tamanho da lista
-    if (media >= 7) {   //se média maior ou igual a 7
-        print(  //mensagem se condição for Verdadeira
+        n.sumOf { it } / n.size //Calcula média dividindo soma do conteudo da lista por tamanho da lista
+    if (media >= 7) {   //Se média maior ou igual a 7
+        print(  //Mensagem se condição for Verdadeira
             "O Aluno foi APROVADO por obter média necessária.\n" +
                     "Média do Aluno: $media"
         )
     } else {
-        print(  //mensagem se condição for Falsa
+        print(  //Mensagem se condição for Falsa
             "O Aluno foi REPROVADO por não obter média necessária.\n" +
                     "Média do Aluno: $media"
         )
@@ -49,5 +50,11 @@ fun MediaAluno() {
 
 //  leia 3 números inteiros e mostre o maior deles.
 fun MaiorDe3() {
-
+    val n = mutableListOf<Int>()    //Criação de lista mutavel
+    for (i in 1..3) {   // Looping com contador de 1 a 3
+        print("Digite um número: ") //Mensagem para usuario
+        val x = readLine().toString().toInt() //Variavel para input
+        n += x //Adiciona o input na lista(n)
+    }
+    print("O maior número digitado foi ${n.max()}") //Output
 }
