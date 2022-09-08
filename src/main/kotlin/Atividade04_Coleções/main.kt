d6eab9e0f50735dc3edef9610f4206c335308424
HelioJr548 <heliojr548@gmail.com>
2022-09-08 20:28:43 -0300

    Segundo exercicio de coleções, finalizado

--- src/main/kotlin/Atividade04_Coleções/main.kt
+++ src/main/kotlin/Atividade04_Coleções/main.kt
@@ -1,8 +1,8 @@
 package Atividade04_Coleções
 
 fun main() {
-    Colecao10()
-
+//    Colecao10()
+    AeB()
 }
 
 //  Criar uma coleção com 10 elementos
@@ -14,4 +14,20 @@ fun Colecao10() {
         a.add(i)
     }
     println("Vetor: $a")
+}
+
+//  Criar uma coleção “A” com 10 elementos
+//  Inserir os elementos de 1 a 10 na coleção
+//  Criar uma outra coleção B que conterá o elemento da coleção A multiplicado por 2
+//  Mostrar as duas coleçõesObs
+fun AeB() {
+    val a = arrayListOf<Int>()
+    val b = arrayListOf<Int>()
+    for (i in 1..10) {
+        a.add(i)
+    }
+    for (i in a) {
+        b.add(i * 2)
+    }
+    print("Vetor A: $a\nVetor B: $b")
 }
